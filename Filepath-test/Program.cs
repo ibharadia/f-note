7cd293ec615eec480a49dd984b492d0214bc69e2
ibharadia <ibha2048@gmail.com>
2017-02-01 17:57:04 -0500

    Add files via upload

---
+++ Filepath-test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FilesystemTesting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fNames = GetFileNames(@"C:\Users\ImranB\Documents\Test");
+            foreach(var name in fNames)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+        private static string[] GetFileNames(string path)
+        {
+            var files = Directory.EnumerateFiles(path); 
+            string[] fileNames = files.ToArray();  
+            return fileNames;  
+        }
+    }
+}
